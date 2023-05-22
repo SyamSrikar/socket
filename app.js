4b44c17df6ad9d0f8a50b8c8f5bc977618c1fde9
SyamSrikar <82269130+SyamSrikar@users.noreply.github.com>
2023-05-22 17:00:30 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const http=require('http');
 const {Server} = require('socket.io')
 const server= http.createServer(app)
-const io =new Server(server,{cors:{origin:'http://localhost:3001',methods:["GET","POST"]}})
+const io =new Server(server,{cors:{origin:'https://srikarsocket.netlify.app/',methods:["GET","POST"]}})
 const cors = require('cors')
 app.use(cors())
 
